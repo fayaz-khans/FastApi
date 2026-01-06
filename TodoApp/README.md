5b951013dd61daeeeb065d9f34df31493e1c1ee2
fayaz-khans <mfayaz00022@gmail.com>
2026-01-06 22:33:33 +0500

    first commit

---
+++ TodoApp/README.md
@@ -0,0 +1,3 @@
+"# Fastapi-The-Complete-Course"
+
+
---
+++ TodoApp/alembic/versions/6ac7cf4ff489_create_a_phone_number_for_user_column.py
@@ -0,0 +1,25 @@
+"""Create a phone number for user column
+
+Revision ID: 6ac7cf4ff489
+Revises: 
+Create Date: 2025-12-22 02:20:59.235290
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '6ac7cf4ff489'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.add_column('users', sa.Column('phone_number', sa.String(), nullable=True))
+
+def downgrade() -> None:
+    pass
